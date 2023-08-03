f681d530cf5b9b6250d201294f98495ac52fb18d
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2023-08-03 15:20:00 -0300

    explicando alguma coisa

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const colors = ['Red', 'Blue', 'Yellow', 'Purple']
 const btn = document.getElementById('btn')
 
 
+// trabalhar no click 
 btn.addEventListener('click', function () {
     const randomColor = getRandomColor()
     document.body.style.background = colors[randomColor]
 
 })
 
+//gerar numeros randomicos 
 function getRandomColor() {
     return Math.floor(Math.random() * colors.length)
 }
